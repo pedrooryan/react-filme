d439de1fb00a0fe2d08e303fdbadee269c55c8e7
Pedro Ryan <reametazgamer3@gmail.com>
2023-04-17 21:54:15 -0300

    segundocommit

--- pages/filmes/index.js
+++ pages/filmes/index.js
@@ -1,14 +1,29 @@
 import Pagina from '@/components/Pagina'
 import apiDeputados from '@/services/apiDeputados'
 import apiFilmes from '@/services/apiFilmes'
+import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
+import { Button, Card, Col, Row } from 'react-bootstrap'
 
 const index = (props) => {
     return (
         <Pagina titulo="Filmes">
-            {props.filmes.map(item => (
-                <p>{item.title}</p>
-            ))}
+
+            <Row md={4}>
+                {props.filmes.map(item => (
+                    <Col>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={"https://image.tmdb.org/t/p/w500" + item.backdrop_path} />
+                            <Card.Body>
+                                <Card.Title>{item.title}</Card.Title>
+                                <p>Lançamento: {item.release_date}</p>
+                                <p>Nota: {item.vote_average}</p>
+                                <Link className='btn btn-danger' href={'/filmes/' + item.id}>Detalhes</Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
         </Pagina>
     )
 }
